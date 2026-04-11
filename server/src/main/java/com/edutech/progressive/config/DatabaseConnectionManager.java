84a5336d1ff64dd519a81e7026be272cad81e935
pranavmakesitcool24k <dummy@gmail.com>
2026-04-11 08:56:11 +0000

    day1

--- server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
+++ server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
@@ -1,5 +1,47 @@
 package com.edutech.progressive.config;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
 public class DatabaseConnectionManager {
 
+    private static Properties properties = new Properties();
+
+    private static void loadProperties() {
+        try (InputStream input =
+                 DatabaseConnectionManager.class
+                     .getClassLoader()
+                     .getResourceAsStream("application.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("application.properties file not found");
+            }
+
+            properties.load(input);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error reading application.properties file", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            if (properties.isEmpty()) {
+                loadProperties();
+            }
+
+            String driver = properties.getProperty("db.driver");
+            String url = properties.getProperty("db.url");
+            String username = properties.getProperty("db.username");
+            String password = properties.getProperty("db.password");
+
+            Class.forName(driver);
+            return DriverManager.getConnection(url, username, password);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create database connection", e);
+        }
+    }
 }
--- server/src/main/java/com/edutech/progressive/entity/Team.java
+++ server/src/main/java/com/edutech/progressive/entity/Team.java
@@ -1,5 +1,69 @@
 package com.edutech.progressive.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Team {
 
+    @Id
+    private int teamId;
+
+    private String teamName;
+    private String location;
+    private String ownerName;
+    private int establishmentYear;
+
+    public Team() {}
+
+    public Team(int teamId, String teamName, String location,
+                String ownerName, int establishmentYear) {
+        this.teamId = teamId;
+        this.teamName = teamName;
+        this.location = location;
+        this.ownerName = ownerName;
+        this.establishmentYear = establishmentYear;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public int getEstablishmentYear() {
+        return establishmentYear;
+    }
+
+    public void setEstablishmentYear(int establishmentYear) {
+        this.establishmentYear = establishmentYear;
+    }
+
+    
 }
