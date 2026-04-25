23557a842a023b945802ba63c3383a77f2bdbbac
Pranav Pardeshi <itsmepranav24k@gmail.com>
2026-04-25 03:51:06 +0000

    day17

--- client/src/app/ipl/components/cricketersample/cricketersample.component.ts
+++ client/src/app/ipl/components/cricketersample/cricketersample.component.ts
@@ -1,4 +1,24 @@
+import { Component } from '@angular/core';
+import { Cricketer } from '../../types/Cricketer';
 
+@Component({
+  selector: 'app-cricketer-sample',
+  standalone: true,
+  templateUrl: './cricketersample.component.html',
+  styleUrls: ['./cricketersample.component.scss']
+})
 export class CricketerSampleComponent {
-  
+
+  cricketer: Cricketer = new Cricketer(
+    1,
+    1,
+    'Virat Kohli',
+    35,
+    'Indian',
+    15,
+    'Batsman',
+    12000,
+    4
+  );
+
 }
--- client/src/app/ipl/components/teamsample/teamsample.component.ts
+++ client/src/app/ipl/components/teamsample/teamsample.component.ts
@@ -1,5 +1,20 @@
+import { Component } from '@angular/core';
+import { Team } from '../../types/Team';
 
-export class TeamSampleComponent  {
+@Component({
+  selector: 'app-team-sample',
+  standalone: true,
+  templateUrl: './teamsample.component.html',
+  styleUrls: ['./teamsample.component.scss']
+})
+export class TeamSampleComponent {
 
-  
+  team: Team = new Team(
+    1,
+    'CSK',
+    'Chennai',
+    'India Cements',
+    2008
+  );
+
 }
